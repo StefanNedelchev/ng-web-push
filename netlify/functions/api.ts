f48e9ca4ec0e7e081928ab8986be05cf3b3fed0a
Stefan <hardmaster.92@hotmail.com>
2024-01-21 18:32:39 +0200

    use in-memory storage

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -2,13 +2,15 @@ import { json } from 'body-parser';
 import cors from 'cors';
 import express, { Router, Request } from 'express';
 import serverless from 'serverless-http';
-import sqlite3 from 'sqlite3';
+// import sqlite3 from 'sqlite3';
 import { PushSubscription as WebPushSubscription, sendNotification, setVapidDetails } from 'web-push';
 
 const vapidKeys = {
   private: 'vJaR1x_O09UIBqmt1owOZDwPBg1-PLsHuanV-0_BH3Y',
   public: 'BLBXQXDsaEO-HGQZZK0a0_1BNA16631qK0kvpj3NbD6p4LKwN6Ks4VPnwuvtYSyR5Yw5qdGIyVLgC_oH1oBIYa8',
 };
+
+let inMemoryDb: WebPushSubscription[] = [];
 
 setVapidDetails('mailto:example@yourdomain.org', vapidKeys.public, vapidKeys.private);
 
@@ -28,71 +30,71 @@ function send(sub: WebPushSubscription) {
   return sendNotification(sub, JSON.stringify(notification));
 }
 
-const db = new sqlite3.Database('subs-db.db', (err) => {
-  if (err) {
-    console.error('DB not initialized.', err);
-    return;
-  }
+// const db = new sqlite3.Database('subs-db.db', (err) => {
+//   if (err) {
+//     console.error('DB not initialized.', err);
+//     return;
+//   }
 
-  db.run(`CREATE TABLE IF NOT EXISTS PUSH_SUBS (
-    endpoint TEXT NOT NULL PRIMARY KEY,
-    p256dh VARCHAR(128) NOT NULL,
-    auth VARCHAR(32) NOT NULL)`);
-});
+//   db.run(`CREATE TABLE IF NOT EXISTS PUSH_SUBS (
+//     endpoint TEXT NOT NULL PRIMARY KEY,
+//     p256dh VARCHAR(128) NOT NULL,
+//     auth VARCHAR(32) NOT NULL)`);
+// });
 
-interface PushSubscriptionRecord {
-  endpoint: string;
-  p256dh: string;
-  auth: string;
-}
+// interface PushSubscriptionRecord {
+//   endpoint: string;
+//   p256dh: string;
+//   auth: string;
+// }
 
-function getAllSubscriptions(): Promise<WebPushSubscription[]> {
-  return new Promise((resolve, reject) => {
-    db.all<PushSubscriptionRecord>('SELECT * FROM PUSH_SUBS', (err, rows) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+// function getAllSubscriptions(): Promise<WebPushSubscription[]> {
+//   return new Promise((resolve, reject) => {
+//     db.all<PushSubscriptionRecord>('SELECT * FROM PUSH_SUBS', (err, rows) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
 
-      const subscriptions = rows.map<WebPushSubscription>((row) => ({
-        endpoint: row.endpoint,
-        keys: { p256dh: row.p256dh, auth: row.auth },
-      }));
-      resolve(subscriptions);
-    });
-  });
-}
+//       const subscriptions = rows.map<WebPushSubscription>((row) => ({
+//         endpoint: row.endpoint,
+//         keys: { p256dh: row.p256dh, auth: row.auth },
+//       }));
+//       resolve(subscriptions);
+//     });
+//   });
+// }
 
-function saveSubscription(sub: WebPushSubscription): Promise<void> {
-  return new Promise((resolve, reject) => {
-    db.run(
-      `INSERT INTO PUSH_SUBS VALUES(:1, :2, :3)
-        ON CONFLICT(endpoint) DO UPDATE SET p256dh=:2, auth=:3`,
-      [sub.endpoint, sub.keys.p256dh, sub.keys.auth],
-      (err) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+// function saveSubscription(sub: WebPushSubscription): Promise<void> {
+//   return new Promise((resolve, reject) => {
+//     db.run(
+//       `INSERT INTO PUSH_SUBS VALUES(:1, :2, :3)
+//         ON CONFLICT(endpoint) DO UPDATE SET p256dh=:2, auth=:3`,
+//       [sub.endpoint, sub.keys.p256dh, sub.keys.auth],
+//       (err) => {
+//         if (err) {
+//           reject(err);
+//           return;
+//         }
 
-        resolve();
-      },
-    );
-  });
-}
+//         resolve();
+//       },
+//     );
+//   });
+// }
 
-function deleteSubscription(endpoint: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    db.run('DELETE FROM PUSH_SUBS WHERE endpoint = ?', endpoint, (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+// function deleteSubscription(endpoint: string): Promise<void> {
+//   return new Promise((resolve, reject) => {
+//     db.run('DELETE FROM PUSH_SUBS WHERE endpoint = ?', endpoint, (err) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
 
-      resolve();
-    });
-  });
-}
+//       resolve();
+//     });
+//   });
+// }
 
 const app = express();
 
@@ -103,39 +105,55 @@ const router = Router();
 
 router.get('/hello', (req, res) => res.send('Hello World!'));
 router.post('/subscribe', (req: Request<unknown, unknown, WebPushSubscription>, res) => {
-  saveSubscription(req.body)
-    .then(() => res.status(200).send({ message: '✅ Subscription saved.' }))
-    .catch((err) => {
-      console.log(err);
-      res.status(422).send({ message: `❌ Subscription was not saved. ${err.message}` });
-    });
+  inMemoryDb.push(req.body);
+  res.status(200).send({ message: '✅ Subscription saved.' });
+  // saveSubscription(req.body)
+  //   .then(() => res.status(200).send({ message: '✅ Subscription saved.' }))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(422).send({ message: `❌ Subscription was not saved. ${err.message}` });
+  //   });
 });
 router.delete('/unsubscripbe', (req: Request<unknown, unknown, WebPushSubscription>, res) => {
-  deleteSubscription(req.body.endpoint)
-    .then(() => res.status(200).send({ message: `❎ Subscription was removed.` }))
-    .catch((err) => {
-      console.log(err);
-      res.status(422).send({ message: `❌ Subscription was not removed. ${err.message}` });
-    });
+  inMemoryDb = inMemoryDb.filter((sub) => sub.endpoint !== req.body.endpoint);
+  res.status(200).send({ message: `❎ Subscription was removed.` });
+  // deleteSubscription(req.body.endpoint)
+  //   .then(() => res.status(200).send({ message: `❎ Subscription was removed.` }))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(422).send({ message: `❌ Subscription was not removed. ${err.message}` });
+  //   });
 });
 router.post('/send-message', (req, res) => {
   setTimeout(() => {
-    getAllSubscriptions()
-      .then((subs) => {
-        const sendPromises = subs.map((sub) => (
-          send(sub).catch((err) => {
-            if (err.statusCode === 404 || err.statusCode === 410) {
-              console.log('Subscription has expired or is no longer valid: ', err.message);
-              deleteSubscription(sub.endpoint);
-            } else {
-              console.error(err);
-            }
-          })
-        ));
-        Promise.all(sendPromises);
-
+    const sendPromises = inMemoryDb.map((sub) => (
+      send(sub).catch((err) => {
+        if (err.statusCode === 404 || err.statusCode === 410) {
+          console.log('Subscription has expired or is no longer valid: ', err.message);
+          inMemoryDb = inMemoryDb.filter((sub) => sub.endpoint !== req.body.endpoint);
+        } else {
+          console.error(err);
+        }
       })
-      .catch((err) => console.error(err))
+    ));
+    Promise.all(sendPromises);
+
+    // getAllSubscriptions()
+    //   .then((subs) => {
+    //     const sendPromises = subs.map((sub) => (
+    //       send(sub).catch((err) => {
+    //         if (err.statusCode === 404 || err.statusCode === 410) {
+    //           console.log('Subscription has expired or is no longer valid: ', err.message);
+    //           deleteSubscription(sub.endpoint);
+    //         } else {
+    //           console.error(err);
+    //         }
+    //       })
+    //     ));
+    //     Promise.all(sendPromises);
+
+    //   })
+    //   .catch((err) => console.error(err))
   }, 5000);
 
   res.status(200).send();
